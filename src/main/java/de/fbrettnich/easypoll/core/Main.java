bf7970925514874b733a5a4da80bf09a23b40bdb
fbrettnich <github@fbrettnich.de>
2021-08-22 21:34:55 +0200

    Added chinese (traditional) translation (Thanks to nado#2213)
    Signed-off-by: fbrettnich <github@fbrettnich.de>

--- src/main/java/de/fbrettnich/easypoll/core/Main.java
+++ src/main/java/de/fbrettnich/easypoll/core/Main.java
@@ -77,7 +77,7 @@ public class Main {
         });
 
         translationManager = new TranslationManager();
-        translationManager.loadTranslations("de-at", "de-de", "en-us", "nl-nl", "pt-br");
+        translationManager.loadTranslations("de-at", "de-de", "en-us", "nl-nl", "pt-br", "zh-tw");
 
         mongodb = new MongoDB(Constants.DEVMODE ? getConfig().getString("mongodb.clienturi_dev") : getConfig().getString("mongodb.clienturi"), getConfig().getString("mongodb.database"));
         mysql = new MySQL(getConfig().getString("mysql.host"), getConfig().getString("mysql.port"), getConfig().getString("mysql.database"), getConfig().getString("mysql.username"), getConfig().getString("mysql.password"));
